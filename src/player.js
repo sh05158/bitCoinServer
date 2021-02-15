c528bf2f2025a5ec7b2bb123dfb50c134d10b221
Joon <sh05158@flysher.net>
2021-02-15 11:50:21 +0900

    error fix

--- src/player.js
+++ src/player.js
@@ -42,10 +42,10 @@ Player.signup = function(msg, cb){
     player.uuid = msg.uuid;
 
     if(!msg.id){
-        msg.id = 'null';
+        msg.id = null;
     }
 
-    sql.query("INSERT INTO player (diamond, isGeneratedNickname, nicknameChangeAvailableCount, nickname, id, platform, level, gold, bitcoin, inventory, equipment, lastLoginTime, chapter, stage, exp, expm, lastRequestTime, vipLevel, vipExp, vipExpm) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+    sql.query("INSERT INTO player (diamond, isGeneratedNickname, nicknameChangeAvailableCount, nickname, id, platform, level, gold, bitcoin, inventory, equipment, lastLoginTime, chapter, stage, exp, expm, lastRequestTime, vipLevel, vipExp, vipExpm) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
         [player.diamond, player.isGeneratedNickname, player.nicknameChangeAvailableCount, player.nickname, msg.id, msg.platform, player.level, player.gold, player.bitcoin, JSON.stringify(player.inventory), JSON.stringify(player.equipment),
         player.lastLoginTime, player.chapter, player.stage, player.exp, player.expm, player.lastRequestTime,player.vipLevel,
     player.vipExp, player.vipExpm],function(err,res){
